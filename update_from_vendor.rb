f26339b8003c3f15ac3e8dcae774f53b98d21227
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-09-13 10:54:06 +0900

    css/js up to -> 0.9.6 see https://github.com/nolimits4web/Framework7/releases

--- update_from_vendor.rb
+++ update_from_vendor.rb
@@ -12,9 +12,7 @@ puts "\n\n\n"
 
 system("wget https://github.com/nolimits4web/Framework7/archive/v#{VERSION_FRAMEWORK7}.tar.gz")
 
-system("tar -zxfv v#{VERSION_FRAMEWORK7}.tar.gz")
-puts "cleaning up downloaded tar"
-system("rm v#{VERSION_FRAMEWORK7}.tar.gz")
+system("tar -zxf v#{VERSION_FRAMEWORK7}.tar.gz")
 
 puts "Copying js files"
 
@@ -36,3 +34,6 @@ system("rm -rf Framework7-#{VERSION_FRAMEWORK7}")
 puts "Finished.."
 puts "You'll need to commit the changes. You should consider updating the change log and gem version number"
 puts "next run install_local.sh"
+
+puts "cleaning up downloaded tar"
+system("rm v#{VERSION_FRAMEWORK7}.tar.gz")
